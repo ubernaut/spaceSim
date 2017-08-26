799c9f61b486adf77bcca673a86b15a829ea3868
Forrest Desjardins <desjardinsfg@gmail.com>
2017-08-25 23:28:35 -0800

    clean up console messages

--- js/simjs/utils.js
+++ js/simjs/utils.js
@@ -1,7 +1,7 @@
 const onProgress = xhr => {
   if (xhr.lengthComputable) {
     const percentComplete = xhr.loaded / xhr.total * 100
-    console.log(`${Math.round(percentComplete, 2)}% downloaded`)
+    Void.log.debug(`${Math.round(percentComplete, 2)}% downloaded`)
   }
 }
 
