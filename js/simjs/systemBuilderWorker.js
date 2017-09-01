f50d860d4e4e80dc48ef2b4b5b89038af433d6c1
Collin Schroeder <collin.schroeder@gmail.com>
2017-09-01 12:43:55 -0800

    Tweaked system generation settings.

--- js/simjs/systemBuilderWorker.js
+++ js/simjs/systemBuilderWorker.js
@@ -2,6 +2,6 @@ import { System } from './systemBuilder'
 
 self.onmessage = e => {
   console.log('in worker')
-  const system = new System(1, 1, 128, 1, 0.02)
+  const system = new System(1, 1, 128, .5, 0.03)
   postMessage(system)
 }
