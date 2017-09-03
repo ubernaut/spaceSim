3ef9eae0e17e61e093abd9bb5fd7bb3a51044459
ubernaut <collin.schroeder@gmail.com>
2017-09-03 09:57:30 -0800

    more planets

--- js/simjs/systemBuilderWorker.js
+++ js/simjs/systemBuilderWorker.js
@@ -2,6 +2,6 @@ import { System } from './systemBuilder'
 
 self.onmessage = e => {
   console.log('in worker')
-  const system = new System(1, 1, 128, .5, 0.03)
+  const system = new System(1, 1, 256, .5, 0.03)
   postMessage(system)
 }
