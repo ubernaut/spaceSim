edb0869f0f54f054630ab4c928c8bf4babdfaa68
Forrest Desjardins <desjardinsfg@gmail.com>
2017-09-04 16:41:36 -0800

    add texture to planets

--- app/js/bodies/planet.js
+++ app/js/bodies/planet.js
@@ -1,12 +1,20 @@
 import { randomUniform } from '-/utils'
+import earthTexture from 'app/assets/images/earthmoon.jpg'
+
+const earthMaterial = new THREE.MeshPhongMaterial({
+  color: randomUniform(0.5, 1) * 0xffffff,
+  fog: true,
+  map: THREE.ImageUtils.loadTexture(earthTexture)
+})
 
 const createPlanet = ({ radius, position }) => {
   const geometry = new THREE.SphereGeometry(radius, 16, 16)
-  const material = new THREE.MeshPhongMaterial({
-    color: randomUniform(0.5, 1) * 0xffffff,
-    fog: true
+
+  const material = earthMaterial.clone()
+  material.color.set(randomUniform(0.5, 1) * 0xffffff)
+  const planet = new THREE.Mesh(geometry, material, {
+    castShadow: true
   })
-  const planet = new THREE.Mesh(geometry, material)
 
   planet.position.x = position.x
   planet.position.y = position.y
