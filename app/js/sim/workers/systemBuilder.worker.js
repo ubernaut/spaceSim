578ead1678bd8d3ee89db2bb14ac01f76e6e900e
ubernaut <collin.schroeder@gmail.com>
2017-09-06 00:30:19 -0800

    increaseed radius where planets are generated from .3 AU to .5 AU

--- app/js/sim/workers/systemBuilder.worker.js
+++ app/js/sim/workers/systemBuilder.worker.js
@@ -1,6 +1,6 @@
 import { System } from '-/sim/systemBuilder'
 
 self.onmessage = e => {
-  const system = new System(1, 1, 256, 0.3, 0.03)
+  const system = new System(1, 1, 256, 0.5, 0.03)
   postMessage(system)
 }
