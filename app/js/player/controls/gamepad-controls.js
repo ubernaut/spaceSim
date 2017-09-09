e69f117491fc8d3474757288dd218501db0d4a7d
Forrest Desjardins <desjardinsfg@gmail.com>
2017-09-08 23:18:12 -0800

    add gamepad-controls

--- app/js/player/controls/gamepad-controls.js
+++ app/js/player/controls/gamepad-controls.js
@@ -32,26 +32,49 @@ const createGamepadControls = (object, domElement) => {
     const moveMult = delta * controls.movementSpeed
 
     const forward = 1
-    controls.movementVector.x = (-controls.movementState.left + controls.movementState.right)
-    controls.movementVector.y = (-controls.movementState.down + controls.movementState.up)
-    controls.movementVector.z = (-forward + controls.movementState.back)
+    controls.movementVector.x = -controls.movementState.left + controls.movementState.right
+    controls.movementVector.y = -controls.movementState.down + controls.movementState.up
+    controls.movementVector.z = -forward + controls.movementState.back
 
     const p1 = gamepads[0]
-    if (p1.buttons[0].pressed) {
-      console.log('b0', controls.object.ship)
-      controls.movementSpeed += 100000
-      controls.object.ship.translateX(controls.movementVector.x * moveMult)
-      controls.object.ship.translateY(controls.movementVector.y * moveMult)
-      controls.object.ship.translateZ(controls.movementVector.z * moveMult)
+    if (p1.buttons[3].pressed) {
+      // console.log('b0', controls.object.ship)
+      controls.movementSpeed += Math.max(1, Math.pow(Math.abs(controls.movementSpeed), 0.85))
+    } else if (p1.buttons[1].pressed) {
+      controls.movementSpeed -= Math.max(1, Math.pow(Math.abs(controls.movementSpeed), 0.85))
     }
 
-    controls.rotationVector.x = p1.axes[0]
-    controls.rotationVector.y = p1.axes[1]
-    controls.rotationVector.z = p1.axes[2]
+    const rotMult = 0.015
+    if (p1.buttons[6].pressed) {
+      // console.log('6')
+      controls.rotationVector.z = -1.5 * p1.buttons[6].value
+    } else if (p1.buttons[7].pressed) {
+      // console.log('7')
+      controls.rotationVector.z = 1.5 * p1.buttons[7].value
+    } else {
+      if (controls.rotationVector.z > 0) {
+        controls.rotationVector.z -= 0.1
+      } else if (controls.rotationVector.z < 0) {
+        controls.rotationVector.z += 0.1
+      }
+    }
 
-    p1.buttons.map(b => {
+    controls.rotationVector.x = 1.0 * p1.axes[1]
+    controls.rotationVector.y = -1.0 * p1.axes[0]
+    // controls.rotationVector.z = p1.axes[2]
+
+    controls.object.ship.translateX(controls.movementVector.x * moveMult)
+    controls.object.ship.translateY(controls.movementVector.y * moveMult)
+    controls.object.ship.translateZ(controls.movementVector.z * moveMult)
+
+    const q = new THREE.Quaternion()
+    q.set(controls.rotationVector.x * rotMult, controls.rotationVector.y * rotMult, controls.rotationVector.z * rotMult, 1).normalize()
+    controls.object.ship.quaternion.multiply(q)
+    controls.object.ship.rotation.setFromQuaternion(controls.object.ship.quaternion, controls.object.ship.rotation.order)
+
+    p1.buttons.map((b, i) => {
       if (b.pressed) {
-        console.log(b)
+        console.log(b, i)
       }
     })
 
