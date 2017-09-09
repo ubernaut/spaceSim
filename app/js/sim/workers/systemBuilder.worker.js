66cce2719ee096eeeaab3dd56689b4ceb53f495d
ubernaut <collin.schroeder@gmail.com>
2017-09-09 01:06:28 -0800

    tripled average planet speed.

--- app/js/sim/workers/systemBuilder.worker.js
+++ app/js/sim/workers/systemBuilder.worker.js
@@ -3,6 +3,6 @@ import { System } from '-/sim/systemBuilder'
 
 self.onmessage = function(e) {
 
-  const system = new System(1, 1, e.data, 0.5, 0.03)
+  const system = new System(1, 1, e.data, 0.5, 0.1)
   postMessage(system)
 }
