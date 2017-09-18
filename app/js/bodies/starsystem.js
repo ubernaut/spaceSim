4b43626e61707fa9b0f4f8e133c009091e201b0e
ubernaut <collin.schroeder@gmail.com>
2017-09-18 13:24:23 -0800

    Added pseusdo code for star system generation

---
+++ app/js/bodies/starsystem.js
@@ -0,0 +1,34 @@
+const createStarSystem = () =>{
+
+  // http://www.schoolsobservatory.org.uk/learn/astro/stars/cycle
+  // galactic epoch
+
+    //if first generation star system
+      //stellar epoch
+        //early: only gas planets, start with no star which then forms one.
+        //mid: could be a massive blue star or quasar
+        //late: supernova which shoots out elements everywhere and collapses to neutron star or black hole.
+
+    //if second generation star system
+       //determine element quantity and distribution  (think of a bell curve over different elements)
+          //stellar epoch:
+            //early:  planetary nebula, no planets, no star or with neutron star/white dwarf core
+            //mid: main sequence star/ massive star
+            //late:  red giant
+            //later: if massive supernova  and either neutron star black hole
+                     // if less massive  planetary nebula and white dwarf
+
+            //single, binary, trinary or n-ary star system
+
+        //system generation,
+        //depending on stellar epoch, stellar energy output, and hill radius, form planets.
+          // if brand new have just evenly distributed nebula everywhere which eventually clumps up into below
+          // if earlyish lots of little planets in chaos (age of bombardment)
+          // if later have a simple coalesced system  with
+              //metallic/sillicate planets close to star
+              // gas giants and asteroids farther out
+              // icy planets and comets farther from that
+              // any planet may have moons depending on its hill radius and whatnot
+
+
+}
