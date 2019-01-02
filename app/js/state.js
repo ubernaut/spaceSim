bfcf2365db0b1cc44b771b3c4112e022f8c9d77c
Forrest Desjardins <desjardinsfg@gmail.com>
2019-01-01 16:46:45 -0900

    add validation for state tree

--- app/js/state.js
+++ app/js/state.js
@@ -1,17 +1,30 @@
 import Baobab from 'baobab'
+import { struct } from 'superstruct'
 
-const schema = {
-  validate: newState => {
-    return { error: null }
-  }
-}
+const Schema = struct({
+  config: {
+    server: {
+      host: 'string',
+      port: 'number'
+    },
+    threejs: {
+      assetPath: 'string'
+    },
+    logging: {
+      name: 'string',
+      level: 'string'
+    }
+  },
+  scene: 'object',
+  controls: 'object'
+})
 
 const state = new Baobab(
   {
     config: {
       server: {
         host: 'https://void-server-0.herokuapp.com/',
-        port: '80'
+        port: 80
       },
       threejs: {
         assetPath: 'app/assets/models/'
@@ -21,26 +34,26 @@ const state = new Baobab(
         level: 'debug'
       }
     },
-
     scene: {
       player: {
         ship: null
       },
       players: []
     },
-
     controls: {
       starType: 'O5'
     }
   },
+
   {
     validate: (previousState, newState, affectedPaths) => {
       if (process.env.NODE_ENV === 'production') {
         return
       }
-      const { error } = schema.validate(newState)
-      if (error) {
-        console.error(error)
+      try {
+        Schema(newState)
+      } catch (err) {
+        console.error(err)
       }
     }
   }
