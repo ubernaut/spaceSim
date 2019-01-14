bc77f2a2261cbce3183cfe69079ec0afa95dd354
Forrest Desjardins <desjardinsfg@gmail.com>
2019-01-13 23:20:04 -0900

    use gzip to serve the site

--- app/server.js
+++ app/server.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const compression = require('compression')
 
 const port = process.env.PORT || 9000
 
 const app = express()
-
+app.use(compression())
 app.use(express.static('dist'))
 app.listen(port, () => console.log(`Listening on port ${port}`))
